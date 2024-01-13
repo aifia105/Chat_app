b226b45e42608d101d471713f86653b744c015e6
Aifia Mohamed <mohamedaifia34@gmail.com>
2024-01-13 02:35:14 +0100

    fix-conv-controller

--- src/main/java/com/aifia/chat/App/controller/ConversationController.java
+++ src/main/java/com/aifia/chat/App/controller/ConversationController.java
@@ -19,17 +19,17 @@ public class ConversationController {
     private final ConversationsService conversationsService;
 
 
-    @PostMapping(value = "conversation/one-on-one/{id}")
+    @PostMapping(value = "/conversation/one-on-one/{id}")
     public ResponseEntity<Conversations> conversationOneOnOne(@PathVariable String id) {
         return ResponseEntity.ok(conversationsService.conversationOneOnOne(id));
     }
 
-    @PostMapping(value = "conversation/group")
+    @PostMapping(value = "/conversation/group")
     public ResponseEntity<Conversations> conversationGroup(@RequestBody List<String> participants) {
         return ResponseEntity.ok(conversationsService.conversationsGroup(participants));
     }
 
-    @GetMapping(value = "conversation/{id}")
+    @GetMapping(value = "/conversation/{id}")
     public ResponseEntity<List<Conversations>> getUserConversation(@PathVariable String id) {
         return ResponseEntity.ok(conversationsService.getUserConversations(id));
     }
